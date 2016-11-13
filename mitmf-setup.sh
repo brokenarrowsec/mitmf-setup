cd8fce250e5606859488b6a045ade2895eb4f060
Sebastian Campos <seabasscampos@gmail.com>
2016-11-13 23:53:01 +0000

    Add files via upload

---
+++ mitmf-setup.sh
@@ -0,0 +1,17 @@
+# Made specifically for Kali Linux 2.0
+# Built by Sebastian Campos (https://github.com/brokenarrowsec)
+# MIT License -- https://github.com/brokenarrowsec/mitmf-setup/blob/master/LICENSE
+#!/bin/bash
+
+git clone https://github.com/
+pip install virtualenvwrapper
+source /usr/local/bin/virtualenvwrapper.sh
+mkvirtualenv MITMf -p /usr/bin/python2.7
+pip install requests[security]
+git clone https://github.com/byt3bl33d3r/MITMf
+cd MITMf && git submodule init && git submodule update --recursive
+pip install -r requirements.txt
+python mitmf.py --help
+
+
+
